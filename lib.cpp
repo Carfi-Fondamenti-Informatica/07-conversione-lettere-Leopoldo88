56a3b0a38a005646fabb146efe2a8ee51e5863bb
Leopoldo88 <114876690+Leopoldo88@users.noreply.github.com>
2022-12-08 19:41:12 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,5 +1,15 @@
-if(65<int (a)<90)||97<int(a)<122){
-toupper(a);  
-return true;
+
+if((int(a)>65&&int(a)<90)||(int(a)>97&&int(a)<122)){
+    if((int(a)>65&&int(a)<90)){
+        a=a-32;
+    }
+    if ((int(a)>97&&int(a)<122)){
+        a=a+32;
+    }
+
+    return true;
 }
-else return false;
+else{
+    return false;
+}
+}
