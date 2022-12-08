137b30e9319b7effc641d8a3d3c8cdf4bb5b404c
Leopoldo88 <114876690+Leopoldo88@users.noreply.github.com>
2022-12-08 21:52:09 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,15 +1,13 @@
-#include"lib.h"
-if((a>'A'&&a<'Z')||(a>'a'&&int(a)<'z')){
-
-    if(a>'A'&&a<'Z'){
-        b=a+32;
-    }
-    if (a>'a'&&int(a)<'z'){
-        c=a-32;
-    }
-    return true;
-}
-else{
+#include "lib.h"
+bool checkinput(int x){
+ if((x>=65&&x<=90)||(x>=97 && x<=122)
+   return true;
+    else
     return false;
-}
-}
+   char convertire(char x){
+      char ris;
+       if(x>= 'A' && x<= 'Z')
+     ris=x+32;
+     else if (x>= 'a' && x<= 'z')
+         ris=z-32;
+       return ris;
