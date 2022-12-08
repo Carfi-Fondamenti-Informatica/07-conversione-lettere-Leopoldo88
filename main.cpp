f24cadde9dfdde65ed15b21d35e49f5086fa2a5a
Leopoldo88 <114876690+Leopoldo88@users.noreply.github.com>
2022-12-08 22:03:07 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,17 +1,15 @@
 #include <iostream>
-#include"lib.h"
+#include "lib.h"
 using namespace std;
 
-int main(){
-    char x,mai, min;
-    cout<<"Inserisci una lettera: ";
-    cin>>x;
-    if(maiuscola(x, mai, min)==true){
-    cout<<boolalpha<<maiuscola(x, mai, min)<<endl;
-    cout<<"La lettera convertita e': "<<min;
-}
-    else{
-    cout<<"No"<<endl;
-} 
-return 0;
-}
+int main() {
+char c;
+    char v;
+    cin>>c;
+    if (checkimput(x:c)){
+        v=convertito(x:c);
+        cout<<v;
+        }else {
+        cout<<"errore"<<endl;
+        }
+    return 0;
