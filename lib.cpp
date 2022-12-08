ef343ebe2703c980854b9e8dcf371a0240771c44
Leopoldo88 <114876690+Leopoldo88@users.noreply.github.com>
2022-12-08 22:59:27 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,19 +1,16 @@
-#include<iostream>
-bool checkinput(char x)
-{
-
- if(((int)x>=65&&x<=90)||((int)x>=97&&x<=122))
-           return true;
-    else
-        return false;
+#include "lib.h"
+bool checkimput(int x){
+  if((x>=65&&x<=90)||(x>=97 && x<=122))
+     return true;
+  else
+     return false;
+     
 }
-char conv(char x)
-{
-    char ris;
-    if(x>='A'&&x<='Z')
-        ris=x+32;
-    else 
-        ris=x-32;
-return ris;
+char conversione(char x){
+  char ris;
+  if (x >= 'A'&& x <= 'Z')
+    ris= x + 32;
+  else if(x>= 'a' && x <= 'z')
+    ris= x - 32;
+  return ris;
 }
-  
