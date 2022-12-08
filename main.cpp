ec575dbe1b3589c525de557af70f68eb670fd7c6
Leopoldo88 <114876690+Leopoldo88@users.noreply.github.com>
2022-12-08 22:39:17 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,15 +1,17 @@
 #include <iostream>
 #include "lib.h"
 using namespace std;
-
-int main() {
+int main()
+{
 char c;
-    char v;
-    cin>>c;
-    if (checkinput(c)){
-        v=conversione(c);
-        cout<<v;
-        }else {
-        cout << "errore" <<endl;
-        }
-    return 0;
+char v;
+cin >> c;
+if(checkinput(c))
+{
+    v=conversione(c);
+    cout<<v;
+}
+else
+    cout << "errore" <<endl;
+return 0;
+}
